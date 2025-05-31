ae3e35a5220c025cc0e29a0367668dd5f08c4828
ibrahim majed alsharhan <i.m.a@IMS-MBA.local>
2025-05-31 16:51:21 +0100

    …

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="border-t bg-neutral-50 py-10 text-center text-sm text-neutral-500">
-      © {new Date().getFullYear()} Acme Inc. All rights reserved.
+      © {new Date().getFullYear()} Masanne Inc. All rights reserved.
     </footer>
   );
 }
